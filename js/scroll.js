63a8daa66bee6265f9f182fd90974a97ab292333
bannermuere <bannermuere@gmail.com>
2021-12-13 13:30:52 -0600

    posicionamiento de cajas

--- js/scroll.js
+++ js/scroll.js
@@ -1,11 +1,13 @@
 
 window.sr = ScrollReveal();
 
-ScrollReveal().reveal('.logo', { duration: 1000 });
-ScrollReveal().reveal('.menu', { delay: 500 });
-ScrollReveal().reveal('.blackMode', { delay: 1000 });
-
-
 ScrollReveal({ reset: true }).reveal('.presentacion');
+
 ScrollReveal({ reset: true }).reveal('.servicios');
 ScrollReveal({ reset: true }).reveal('.servicio', { interval: 200 });
+
+ScrollReveal({ reset: true }).reveal('.portafolio');
+
+ScrollReveal({ reset: true }).reveal('.contenedorContacto');
+ScrollReveal({ reset: true }).reveal('.formularioContacto', { delay: 200 });
+
