3f62e23335f8002bd9f68ffc440ddf964cc0855b
bannermuere <bannermuere@gmail.com>
2021-12-13 11:46:57 -0600

    animaciones

---
+++ js/scroll.js
@@ -0,0 +1,9 @@
+
+ScrollReveal({ duration: 1000 })
+
+ScrollReveal({ reset: true }).reveal('.logo');
+ScrollReveal({ reset: true }).reveal('.menu', { delay: 500 });
+ScrollReveal({ reset: true }).reveal('.blackMode', { delay: 1000 });
+
+
+ScrollReveal({ reset: true }).reveal('.servicios');
