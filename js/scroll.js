a9a9f18f713c0f82d29a0173a4062bd80e597d57
bannermuere <bannermuere@gmail.com>
2021-12-13 12:26:25 -0600

    animaciones 2

--- js/scroll.js
+++ js/scroll.js
@@ -1,9 +1,11 @@
 
-ScrollReveal({ duration: 1000 })
+window.sr = ScrollReveal();
 
-ScrollReveal({ reset: true }).reveal('.logo');
-ScrollReveal({ reset: true }).reveal('.menu', { delay: 500 });
-ScrollReveal({ reset: true }).reveal('.blackMode', { delay: 1000 });
+ScrollReveal().reveal('.logo', { duration: 1000 });
+ScrollReveal().reveal('.menu', { delay: 500 });
+ScrollReveal().reveal('.blackMode', { delay: 1000 });
 
 
+ScrollReveal({ reset: true }).reveal('.presentacion');
 ScrollReveal({ reset: true }).reveal('.servicios');
+ScrollReveal({ reset: true }).reveal('.servicio', { interval: 200 });
